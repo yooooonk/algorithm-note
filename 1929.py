35c09a4dcb8fa427da4cbdbac6a055d48027c87a
ouo_yoonk <joyk9251@gmail.com>
2021-03-08 21:34:01 +0900

    소수구하기

--- 1929.py
+++ 1929.py
@@ -2,17 +2,33 @@
 # 입력 받은 수의 배수를 빼보자
 # 어떻게? 배열 여러번 순회? xN
 
-a,b = map(int,input().split())
-fullArray = list(range(b+1))[a:]
 
-def is_prime(i):
-    if(i<=1) : return
+a, b = map(int, input().split())
 
-    n=2
-    while n*n <= i:
-        if i%n == 0: return
-        n+= 2
-    print(i)
+def isPrime(num) :
+    if num==1:
+        return False
+    else:
+        for i in range(2,int(num**0.5)+1):
+            if num%i == 0:
+                return False
+            return True
 
-for i in fullArray :
-    is_prime(i)
+for i in range(a,b+1) :
+    if isPrime(i):
+        print(i)
+
+""" def isPrime(num):
+    if num==1:
+        return False
+    else:
+        for i in range(2, int(num**0.5)+1):
+            if num%i == 0:
+                return False
+        return True
+
+M, N = map(int, input().split())
+
+for i in range(M, N+1):
+    if isPrime(i):
+        print(i)  """
