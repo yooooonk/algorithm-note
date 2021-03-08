4073cea067baf62e33e467bcbbaf12ad34c8ca31
ouo_yoonk <joyk9251@gmail.com>
2021-03-08 22:03:43 +0900

    소수구하기 - 수정

--- 1929.py
+++ 1929.py
@@ -3,20 +3,20 @@
 # 어떻게? 배열 여러번 순회? xN
 
 
-a, b = map(int, input().split())
+m, n = map(int, input().split())
 
-def isPrime(num) :
-    if num==1:
-        return False
-    else:
-        for i in range(2,int(num**0.5)+1):
-            if num%i == 0:
-                return False
-            return True
+n+=1
+prime = [True]*n
+for i in range(2,int(n**0.5)+1):
+    if prime[i]:
+        for j in range(2*i,n,i):
+            prime[j] = False
 
-for i in range(a,b+1) :
-    if isPrime(i):
+for i in range(m,n):
+    if i >1 and prime[i] == True:
         print(i)
+
+
 
 """ def isPrime(num):
     if num==1:
@@ -32,3 +32,4 @@ M, N = map(int, input().split())
 for i in range(M, N+1):
     if isPrime(i):
         print(i)  """
+
