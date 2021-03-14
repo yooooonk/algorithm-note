dfcc23812d93b2e2439c42f7a5d4ca45baa096a1
ouo_yoonk <joyk9251@gmail.com>
2021-03-14 18:04:21 +0900

    파도반 수열

---
+++ 9461.py
@@ -0,0 +1,21 @@
+# 파도반 수열
+# https://www.acmicpc.net/problem/9461
+
+import sys
+n = int(sys.stdin.readline())
+numbers = []
+dp = [0,1,1,1,2,2]
+
+def getP(N):    
+    last = len(dp)-1 
+    if N > last:
+        for t in range(last+1,N+1):
+            dp.append(dp[t-1]+dp[t-5])
+    print(dp[N])
+
+for _ in range(n):
+    numbers.append(int(input()))
+
+for i in numbers :
+    getP(i)
+
