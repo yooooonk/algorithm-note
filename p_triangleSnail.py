c166da56d979d8fb4ccb7dbda6f3f7e5a0bc4ed4
ouo_yoonk <joyk9251@gmail.com>
2021-05-16 14:12:17 +0900

    프로그래머스 삼각 달팽이

---
+++ p_triangleSnail.py
@@ -0,0 +1,31 @@
+def solution(n):
+    answer = []
+    
+    # 이차원배열 만들기
+    snail = [[0]*n for _ in range(n)]          
+    num = 1
+    x,y = -1,0
+
+    for i in range(n) :
+        for j in range(i,n) :
+            if i%3 == 0:
+                x += 1
+
+            elif i%3 == 1 :
+                y += 1
+
+            elif i%3 == 2 :
+                x -= 1
+                y -= 1
+            
+            snail[x][y] = num
+            num += 1
+
+    for i in snail :
+        for j in i :
+            if(j != 0) :
+                answer.append(j)
+            
+    return answer
+
+print(solution(7))
