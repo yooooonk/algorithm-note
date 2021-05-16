83b9b40110cc3dad17ce2b5caafebd1602bb079d
ouo_yoonk <joyk9251@gmail.com>
2021-05-16 10:42:00 +0900

    프로그래머스 소수찾기

---
+++ p_primeSearch.py
@@ -0,0 +1,26 @@
+from itertools import permutations
+import math
+
+def isPrime(n) :
+    if n<2 :
+        return False
+    q = math.sqrt(n)
+    for i in range(2, int(q)+1) :
+        if n%i == 0 :
+            return False
+    return True
+        
+
+def solution(numbers):
+    numberList = list(numbers)
+    answer = []
+
+    # 모든 경우의 수
+    for i in range(1,len(numbers)+1) :
+        perlist = list(set(map(int,list(map(''.join,permutations(numberList,i))))))
+        for p in perlist :   
+            if(isPrime(p)):
+                answer.append(p)   
+    return len(set(answer))
+
+print(solution('011'))    
