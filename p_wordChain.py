552f8e0d50a12e77aabc36f672b10334f8224a78
ouo_yoonk <joyk9251@gmail.com>
2021-05-18 10:07:29 +0900

    프로그래머스 영어 끝말잇기

---
+++ p_wordChain.py
@@ -0,0 +1,18 @@
+def solution(n, words):
+    answer = [0,0]
+
+    for i in range(1,len(words)) : 
+        # 중복글자
+        if words[i-1][-1] != words[i][0] or words.index(words[i]) < i :
+            print(i)
+            
+            num = (i%n)+1
+            cnt = (i+1)//n if (i+1)%n==0 else (i+1)//n+1
+            answer = [num,cnt]                       
+            break;
+
+    return answer
+
+n = 3
+words = ["hello", "one", "even", "ggnever", "now", "world", "draw"]
+print(solution(n,words))    
