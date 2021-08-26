02549adb055eceedf1c6b747e63dad8287b61494
gisele <gisele>
2021-08-26 13:40:11 +0900

    [그리디]2217.로프 - 푸는 중

--- 5585.py
+++ 5585.py
@@ -1,3 +1,6 @@
+# [그리디] 5585.거스름돈
+# https://www.acmicpc.net/problem/5585
+
 import sys
 
 input = sys.stdin.readline
---
+++ greedy_2217.py
@@ -0,0 +1,13 @@
+# [그리디] 2217. 로프
+# https://www.acmicpc.net/problem/2217
+
+import sys
+input = sys.stdin
+
+n = int(input())
+arr = []
+
+for _ in range(n) :
+    arr.append(int(input()))
+
+print(arr)
