1a9ff593ad8f5a8182537ebddd1527b931693a28
gisele <gisele>
2021-08-30 13:18:14 +0900

    [그리디]2217.로프 - 완료

--- greedy_2217.py
+++ greedy_2217.py
@@ -2,7 +2,7 @@
 # https://www.acmicpc.net/problem/2217
 
 import sys
-input = sys.stdin
+input = sys.stdin.readline
 
 n = int(input())
 arr = []
@@ -10,4 +10,9 @@ arr = []
 for _ in range(n) :
     arr.append(int(input()))
 
-print(arr)
+# w = 개수 * 무게
+
+arr.sort(reverse = True)
+for i in range(n) :
+     arr[i] = arr[i] * (i+1)
+print(max(arr))
