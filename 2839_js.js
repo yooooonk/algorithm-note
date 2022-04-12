46b1d797186271e9aed040feab618b395b8456f5
gisele <gisele>
2022-04-12 09:11:10 +0900

    2839 js 설탕배달 - 5a+3b 문제는 while문 이용해 :wq!
    :wq!
    
    1
    ::w!
    :wq!

---
+++ 2839_js.js
@@ -0,0 +1,34 @@
+// 설탕 배달
+// https://www.acmicpc.net/problem/2839
+
+let input = require('fs').readFileSync('dev/stdin.txt').toString().split('\n')
+const sugar = parseInt(input[0])
+
+console.log(solution(sugar))
+
+function solution(sugar){
+    let count = 0;
+    let rSugar = sugar;
+    while(rSugar>=0){
+
+
+        if(rSugar%5===0){
+            const n = Math.floor(rSugar/5)
+
+            count += n
+            console.log('break',count)
+            rSugar -= 5*n;
+            break;
+        }
+
+        rSugar -= 3;
+        count ++;
+
+    }
+
+    if(rSugar!==0){
+        count = -1;
+    }
+
+    return count;
+}
